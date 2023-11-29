1c1aa11d21b4e43d8493bc491636ba1971f15d4d
aslujan99 <151554641+aslujan99@users.noreply.github.com>
2023-11-29 18:39:17 +0100

    Update streamlit_app.py
    Añadimos variable.

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ if streamlit.button('Selecciona Fruta de la lista'):
 
 def insert_row_snowflake(new_fruit):
 	with my_cnx.cursor() as my_cur:
-		my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+		my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
 		my_cur.close()
 		return "Thanks for adding " + new_fruit
 
