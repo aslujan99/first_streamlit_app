b4401318b476c04b356b45da3bf2d251c654f7c6
aslujan99 <151554641+aslujan99@users.noreply.github.com>
2023-11-29 17:48:26 +0100

    Update streamlit_app.py
    fix bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ except URLError as e:
       streamlit.stop()
 ################################################
 
-streamlit.stop()
+#streamlit.stop()
 
 def get_fruit_load_list():
 	with my_cnx.cursor() as my_cur: 	
