47caae07ea9e0c09b649df53298a44be1d29de11
aslujan99 <151554641+aslujan99@users.noreply.github.com>
2023-11-29 17:16:26 +0100

    Update streamlit_app.py
    fix bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.dataframe(fruits_to_show)
 #Creamos una funcion
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + (this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalizefruityvice_response ())
+    fruityvice_normalized = pandas.json_normalizefruityvice_response ()
     return fruityvice_normalized
   
 ################################################
